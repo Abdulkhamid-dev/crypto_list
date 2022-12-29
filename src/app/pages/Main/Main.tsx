c00e5c8a7fa1a494feb36b5ded53838aab98dcd2
Abdulkhamid <abdulkhamid1828@gmail.com>
2022-12-30 00:23:02 +0500

    add calculation with previous value

--- src/app/pages/Main/Main.tsx
+++ src/app/pages/Main/Main.tsx
@@ -1,61 +1,124 @@
-import React, { useState, useEffect } from "react";
-import { Select, Form, Button, Layout, Table } from "antd";
+import React, { useState, useEffect, useRef } from "react";
+import { Select,  Button, Layout, Table, message } from "antd";
 import { StyledMain } from "./Main.style";
 import Logo from "../../../assets/img/crypto_logo.png";
 import axios from "axios";
+import {
+  TiArrowSortedDown,
+  TiArrowSortedUp,
+} from "react-icons/ti";
 type TCoin = {
   name: string;
-  value: number;
+  USD: number;
 };
 const { Header, Content, Footer } = Layout;
 function Main() {
+  const prevCoins = useRef<TCoin[]>([]);
   const [searchVal, setSearchVal] = useState<string>("");
-  const [coinList, setCoinList] = useState<string[]>(["DOGE"]);
-  const [coinData, setCoinData] = useState<any[]>([]);
+  const [coinList, setCoinList] = useState<string[]>(["DOGE", "BTC"]);
+  const [coinData, setCoinData] = useState<TCoin[]>([]);
 
   const handleSearch = () => {
     if (coinList.includes(searchVal)) {
       alert("Already exist");
+    } else {
+      setCoinList([...coinList, searchVal]);
     }
-    setCoinList([...coinList, searchVal]);
   };
 
-  console.log(coinList);
-
-  const sendReq = async () => {
+  const getCoin = async (coin: string) => {
     try {
       const { data } = await axios.get(
-        `https://min-api.cryptocompare.com/data/pricemulti?fsyms=${coinList}&tsyms=USD&api_key=4467f9c263b8d45582b456a0d5e9fe7189406c9234c8e7c731db239b904de84d&gt`
+        `https://min-api.cryptocompare.com/data/price?fsym=${coin}&tsyms=USD&api_key=4467f9c263b8d45582b456a0d5e9fe7189406c9234c8e7c731db239b904de84d&gt`
       );
-      console.log(data);
-      setCoinData(data);
-    } catch (error) {
-      console.log(error);
-    }
+      return data;
+    } catch (error) {}
   };
 
-  useEffect(() => {});
+  const getData = async () => {
+    let result: TCoin[] = [];
+    coinList.forEach(async (coin) => {
+      let data = await getCoin(coin);
+      result.push({ name: coin, USD: data.USD });
+    });
+    setTimeout(() => {
+      setCoinData(result);
+    }, 1000);
+  };
+
+  const calcWithPrev = (coin: TCoin) => {
+    if (prevCoins.current) {
+      let currentCoin = prevCoins.current.find(
+        (item) => item.name === coin.name
+      );
+      console.log(currentCoin);
+      if (currentCoin) {
+        return coin.USD > currentCoin.USD ? (
+          <TiArrowSortedUp color="green" />
+        ) : coin.USD < currentCoin.USD ? (
+          <TiArrowSortedDown color="red" />
+        ) : (
+          "-"
+        );
+      } else {
+        return "-";
+      }
+    } else {
+      return "-";
+    }
+  };
 
   const columns = [
     {
+      title: "#",
+      dataIndex: "",
+      width: 50,
+      key: "number",
+      render: (record: any, text: any, index: number) => {
+        return <h5>{index + 1}</h5>;
+      },
+    },
+    {
       title: "Name",
-      dataIndex: "name",
+      dataIndex: "",
       key: "name",
+      render: (record: any, text: any) => {
+        return <h5>{record.name}</h5>;
+      },
     },
     {
       title: "In USD",
-      dataIndex: "value",
+      dataIndex: "",
       key: "value",
+      render: (record: any, text: any) => {
+        return <h5>{record?.USD}</h5>;
+      },
+    },
+    {
+      title: "In Graphic",
+      dataIndex: "",
+      key: "graphics",
+      render: (record: any, text: any) => {
+        return calcWithPrev(record);
+      },
     },
   ];
   useEffect(() => {
-    sendReq();
-  }, [coinList]);
-  useEffect(() => {
-    setInterval(() => {
-      sendReq();
+    getData();
+    const interval = setInterval(() => {
+      getData();
     }, 5000);
+    return () => clearInterval(interval);
+  }, [coinList]);
+
+  useEffect(() => {
+    prevCoins.current = coinData;
+  }, [coinData]);
+
+  useEffect(() => {
+    getData();
   }, []);
+
   return (
     <StyledMain>
       <header></header>
@@ -126,9 +189,7 @@ function Main() {
         </Header>
         <Content className="site-layout">
           <div className="main_content" style={{ minHeight: 600 }}>
-            <Button>Get</Button>
-            {/* <Table columns={columns} dataSource={coinData} /> */}
-            {JSON.stringify(coinData)}
+            <Table columns={columns} dataSource={coinData} />
           </div>
         </Content>
       </Layout>
